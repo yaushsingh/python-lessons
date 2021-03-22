0503d8f8700a6927f5820a1d3c599dfa9a58bd99
yaushsingh <yaushsingh2077@gmail.com>
2021-03-22 22:04:39 +0545

    modified logger

--- loggers.py
+++ loggers.py
@@ -2,17 +2,25 @@ import logging
 #logging.basicConfig(format = '%(asctime)s %(levelname)s : %(message)s' , level = logging.DEBUG) #changing the default level of logging to DEBUG
 #format can show date and remove the logger name
 #writing more readable code
-logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d]%(message)s', level =logging.DEBUG)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d]%(message)s', 
+    datefmt = '%d-%m-%Y %H: %M: %S',
+    level =logging.DEBUG,
+    filename = 'my_logs.txt' #writing and saving the logs in different files
+    )
 # -*s gives 8 spcaes for the level name
 #()s is string format for python
 
+logger = logging.getLogger('books')
 #initiate a logger
-logger = logging.getLogger('test_logger') #getLogger is class of the module logging
+"""logger = logging.getLogger('__name__')"""
+# __name__  to have  different logger for seperate modules
+ #getLogger is class of the module logging
 #can be used in different file
 
 logger.info('this will not show up')
 logger.warning('This shows warning')
-
+logger.debug('this is a debug message')
 """
 level of logging
 DEBUG (used while developing phase)
@@ -20,4 +28,7 @@ INFO
 WARNING
 ERROR
 CRITICAL
-"""
+""" 
+logger = logging.getLogger('books.database') 
+#above logger is child logger of books 
+#any  config on books logger will be inherited by database logger
